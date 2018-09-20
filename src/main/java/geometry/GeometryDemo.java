c8435831d5976512e0cd2c962eb3e26927cd3dca
svolkov <svolkovua@gmail.com>
2018-09-20 23:19:53 +0300

    Introducing area calculation method in the class Parallelogram.java

--- src/main/java/geometry/GeometryDemo.java
+++ src/main/java/geometry/GeometryDemo.java
@@ -1,4 +1,12 @@
 package geometry;
 
+import geometry.figures.Parallelogram;
+import java.lang.*;
+
 public class GeometryDemo {
+
+    public static void main(String[] args) {
+        Parallelogram parallelogram1 = new Parallelogram(10, 8, 90);
+        System.out.println(parallelogram1.calculateArea());
+    }
 }
--- src/main/java/geometry/figures/Parallelogram.java
+++ src/main/java/geometry/figures/Parallelogram.java
@@ -1,4 +1,18 @@
 package geometry.figures;
 
 public class Parallelogram {
+    private int baseSide;
+    private int edgeSide;
+    private int angle;
+
+    public Parallelogram( int baseSide, int edgeSide, int angle ){
+        this.angle = angle;
+        this.baseSide = baseSide;
+        this.edgeSide = edgeSide;
+    }
+
+    public int calculateArea(){
+        return (int) Math.round(baseSide * edgeSide * Math.sin(Math.toRadians( angle )));
+
+    }
 }
